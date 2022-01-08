e6f1b3b5e3569b063f13009e7646b17476a6e953
Janelle Peisley <jpeisle2@myune.edu.au>
2022-01-08 13:00:19 +1100

    Scoring started

--- php/process_results.php
+++ php/process_results.php
@@ -3,12 +3,69 @@
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
+// VARIABLES TO SCORE RUNNING TOTAL
+
+$results= array(
+    "animalJustice" => 0
+/*
+$australiaFirst = 0;
+$australianAffordableHousing = 0;
+$australianChristians = 0;
+$australianCitizens = 0;
+$australianDemocrats = 0;
+$australianFederation = 0;
+$australianGreens = 0;
+$australianLaborParty = 0;
+$australianProgressives = 0;
+$centreAlliance = 0;
+$christianDemocratic = 0;
+$climateEmergancy = 0;
+$countryLiberal = 0;
+$democraticLabour = 0;
+$derrynHinch = 0;
+$federalICAC = 0;
+$healthAustralia = 0;
+$hemp = 0;
+$independentsCan = 0;
+$informedMedicalOptions = 0;
+$jaquiLambie = 0;
+$kattersAustralian = 0;
+$liberalDemocratic = 0;
+$liberalParty = 0;
+$loveAustraliaOrLeave = 0;
+$nationalParty = 0;
+$no5G = 0;
+$oneNation = 0;
+$reasonAustralia = 0;
+$rexPatrick = 0;
+$scienceParty = 0;
+$secularParty = 0;
+$shootersFishers = 0;
+$sociallistAlliance = 0;
+$socialistEquality = 0;
+$sustainableAustralia = 0;
+$greatAustralian = 0;
+$newLiberals = 0;
+$transportMatters = 0;
+$unitedAustralia = 0;
+$voteFlux = 0;
+$victorianSocialists = 0;
+$westernAustralia = 0;
+*/
+);
+
+
+
+
+
+
+// SERVER RESPONSE TO POST REQUEST
 
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     // copy returned json into an array?
     $array = json_decode($_POST['results'], true);
 
-    $results= [];
+    
 
     foreach($array as $value => $answer) { 
 
@@ -21,7 +78,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
                 switch($answer['result']){
                     case "stronglyAgree":
                         // do something
-                        array_push($results, "100"); 
                         break;
                     case "somewhatAgree":
                         // do something
@@ -41,15 +97,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 
             break;
             
-            // END TEMPLATE ****************************************
+            // END TEMPLATE *******************************************
             
 
             case "X1":   
                 
                 switch($answer['result']){
                     case "stronglyAgree":
-                        // do something
-                        array_push($results, "100"); 
+                        // do something 
+                        $results['animalJustice']++;
                         break;
                     case "somewhatAgree":
                         // do something
@@ -71,13 +127,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 
 
             case "Z1":
-                array_push($results, "80");    
+                //array_push($results, "80");    
                 break;
                 
             default:
                 // do nothing
         }
+    
         
+    /*
+    $results = array_push_assoc($results, $australiaFirst, $australiaFirst);
+
+
+    function array_push_assoc($array, $key, $value){
+        $array[$key] = $value;
+        return $array;
+     }
+    */
         
     }
     // Send something back
